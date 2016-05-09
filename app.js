5782e93cd384d7b98bf9a89c491c8b4e9e2fc75f
Shai Shiff <shai@crossrider.com>
2016-05-09 16:26:50 +0300

    - starting up

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
 var Botkit = require('botkit');
 var controller = Botkit.facebookbot({
-  access_token: process.env.access_token,
-  verify_token: process.env.verify_token,
+  access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
+  verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
 })
 
 var bot = controller.spawn({});
 
 // if you are already using Express, you can use your own server instance...
 // see "Use BotKit with an Express web server"
-controller.setupWebserver(process.env.port, function(err, webserver) {
+controller.setupWebserver(process.env.PORT, function(err, webserver) {
   controller.createWebhookEndpoints(controller.webserver, bot, function() {
     console.log('This bot is online!!!');
   });
