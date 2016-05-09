01c624a8a2b40696970a0272c5ea25f8384a5904
Shai Shiff <shai@crossrider.com>
2016-05-09 16:16:46 +0300

    - first real commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+var Botkit = require('botkit');
+var controller = Botkit.facebookbot({
+  access_token: process.env.access_token,
+  verify_token: process.env.verify_token,
+})
+
+var bot = controller.spawn({});
+
+// if you are already using Express, you can use your own server instance...
+// see "Use BotKit with an Express web server"
+controller.setupWebserver(process.env.port, function(err, webserver) {
+  controller.createWebhookEndpoints(controller.webserver, bot, function() {
+    console.log('This bot is online!!!');
+  });
+});
+
+// this is triggered when a user clicks the send-to-messenger plugin
+controller.on('facebook_optin', function(bot, message) {
+  bot.reply(message, 'Welcome to my app!');
+});
+
+// user said hello
+controller.hears(['hello'], 'message_received', function(bot, message) {
+  bot.reply(message, 'Hey there.');
+});
+
+controller.hears(['cookies'], 'message_received', function(bot, message) {
+  bot.startConversation(message, function(err, convo) {
+    convo.say('Did someone say cookies!?!!');
+    convo.ask('What is your favorite type of cookie?', function(response, convo) {
+      convo.say('Golly, I love ' + response.text + ' too!!!');
+      convo.next();
+    });
+  });
+});
